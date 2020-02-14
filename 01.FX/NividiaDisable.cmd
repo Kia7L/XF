fd7bdb51fbce56ba47aacd1df7e703ba37ce7b79
Kiaaina Mederios <42665289+Kia7L@users.noreply.github.com>
2020-02-13 14:37:50 -1000

    fix doesnt work tho anymore

---
+++ 01.FX/NividiaDisable.cmd
@@ -0,0 +1,19 @@
+cd /
+@echo off
+mode 75,10
+@Color 01
+ cls
+set "NameofService1=NVIDIA Display Container LS" 
+set "service=service"
+set "action1=stop"
+set "action2=disabled"
+set "nircmd=C:\Windows\nircmd.exe"
+set "cecho=C:\Windows\cecho.exe"
+cls
+%cecho% {01} Product:{\n}
+%nircmd% %service% %action1% %NameofService1%
+%nircmd% %service% %action2% %NameofService1%
+%cecho% {04} %nircmd% %service% %action1% %NameofService1% {\n}
+%cecho% {09} %nircmd% %service% %action2% %NameofService1% {\n}
+(timeout /t 4)>nul
+exit
